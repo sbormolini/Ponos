f989b2094514dd75b5bd57ec2331401e843dbe62
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-06-07 23:13:20 +0200

    Added sample blazor frontend

---
+++ frontend/TodoItem.cs
@@ -0,0 +1,10 @@
+public class TodoItem
+{
+    public string? Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public bool Completed { get; set; }
+}
