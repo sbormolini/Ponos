3a7c4bad97ae10afdd7d3b70c6bbab4ca4b770cb
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-06-07 23:04:41 +0200

    Added backend api

---
+++ backend/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Bos.Todo.Api.Models;
+
+public class Item
+{
+    [JsonProperty(PropertyName = "id")]
+    public string? Id { get; set; }
+
+    [JsonProperty(PropertyName = "name")]
+    public string? Name { get; set; }
+
+    [JsonProperty(PropertyName = "description")]
+    public string? Description { get; set; }
+
+    [JsonProperty(PropertyName = "isComplete")]
+    public bool Completed { get; set; }
+}
---
+++ backend/Program.cs
@@ -0,0 +1,75 @@
+using Microsoft.Azure.Cosmos;
+
+namespace Bos.Todo.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // CosmosDB info
+        var accountEndpoint = builder.Configuration.GetSection("CosmosDB").GetValue<string>("AccountEndpoint");
+        var databaseName = builder.Configuration.GetSection("CosmosDB").GetValue<string>("DatabaseName");
+        var containerName = builder.Configuration.GetSection("CosmosDB").GetValue<string>("ContainerName");
+        var key = builder.Configuration.GetSection("CosmosDB").GetValue<string>("Key");
+
+        // Add services to the container.
+        builder.Services.AddSingleton<ICosmosDbService>(
+            InitializeCosmosClientInstanceAsync(accountEndpoint, databaseName, containerName, key).GetAwaiter().GetResult());
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    /// <summary>
+    /// Creates a Cosmos DB database and a container with the specified partition key. 
+    /// </summary>
+    /// <returns></returns>
+    private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(
+        string accountEndpoint, 
+        string databaseName, 
+        string containerName, 
+        string key)
+    {
+        // for local dev
+        CosmosClientOptions cosmosClientOptions = new CosmosClientOptions()
+        {
+            HttpClientFactory = () =>
+            {
+                HttpMessageHandler httpMessageHandler = new HttpClientHandler()
+                {
+                    ServerCertificateCustomValidationCallback = (req, cert, chain, errors) => true
+                };
+                return new HttpClient(httpMessageHandler);
+            },
+            ConnectionMode = ConnectionMode.Gateway
+        };
+
+        CosmosClient client = new(accountEndpoint, key, cosmosClientOptions);
+        CosmosDbService cosmosDbService = new(client, databaseName, containerName);
+        DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+        await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+        return cosmosDbService;
+    }
+}
