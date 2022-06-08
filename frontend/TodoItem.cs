61e3f32774e4f53562f551271a674ca24b35b75a
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-06-08 17:56:31 +0200

    Added api fetch page

--- frontend/TodoItem.cs
+++ frontend/TodoItem.cs
@@ -1,10 +1,7 @@
 public class TodoItem
 {
     public string? Id { get; set; }
-
     public string? Name { get; set; }
-
     public string? Description { get; set; }
-
-    public bool Completed { get; set; }
+    public bool IsComplete { get; set; }
 }
