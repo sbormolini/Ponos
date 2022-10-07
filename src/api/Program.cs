32083cb164f680a5c412d992a1d44a9ba9b75898
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-10-07 22:18:23 +0200

    Refactor Api

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,83 +1,46 @@
+using Azure.Identity;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.Azure.Cosmos;
+using Ponos.Api;
 
-namespace Bos.Todo.Api;
+var credential = new DefaultAzureCredential();
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddSingleton<ListsRepository>();
+builder.Services.AddSingleton(_ => new CosmosClient(builder.Configuration["AZURE_COSMOS_ENDPOINT"], credential, new CosmosClientOptions()
 {
-    public static void Main(string[] args)
+    SerializerOptions = new CosmosSerializationOptions
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // CosmosDB info
-        var accountEndpoint = builder.Configuration.GetSection("CosmosDB").GetValue<string>("AccountEndpoint");
-        var databaseName = builder.Configuration.GetSection("CosmosDB").GetValue<string>("DatabaseName");
-        var containerName = builder.Configuration.GetSection("CosmosDB").GetValue<string>("ContainerName");
-        var key = builder.Configuration.GetSection("CosmosDB").GetValue<string>("Key");
-
-        // Add services to the container.
-        builder.Services.AddSingleton<ICosmosDbService>(
-            InitializeCosmosClientInstanceAsync(accountEndpoint, databaseName, containerName, key).GetAwaiter().GetResult());
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        // cors
-        //policy.WithOrigins("http://localhost:5166", "https://localhost:7265")
-        app.UseCors(policy =>
-            policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader());
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
     }
+}));
+builder.Services.AddControllers();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration);
 
-    /// <summary>
-    /// Creates a Cosmos DB database and a container with the specified partition key. 
-    /// </summary>
-    /// <returns></returns>
-    private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(
-        string accountEndpoint, 
-        string databaseName, 
-        string containerName, 
-        string key)
-    {
-        //// for local dev
-        //CosmosClientOptions cosmosClientOptions = new CosmosClientOptions()
-        //{
-        //    HttpClientFactory = () =>
-        //    {
-        //        HttpMessageHandler httpMessageHandler = new HttpClientHandler()
-        //        {
-        //            ServerCertificateCustomValidationCallback = (req, cert, chain, errors) => true
-        //        };
-        //        return new HttpClient(httpMessageHandler);
-        //    },
-        //    ConnectionMode = ConnectionMode.Gateway
-        //};
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-        //CosmosClient client = new(accountEndpoint, key, cosmosClientOptions);
-        CosmosClient client = new(accountEndpoint, key);
-        CosmosDbService cosmosDbService = new(client, databaseName, containerName);
-        DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
-        await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+var app = builder.Build();
 
-        return cosmosDbService;
-    }
+app.UseCors(policy =>
+{
+    policy.AllowAnyOrigin();
+    policy.AllowAnyHeader();
+    policy.AllowAnyMethod();
+});
+
+// Swagger UI
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseStaticFiles(new StaticFileOptions{
+    // Serve openapi.yaml file
+    ServeUnknownFileTypes = true,
+});
+
+app.MapControllers();
+app.Run();
