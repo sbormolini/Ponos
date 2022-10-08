06407ad59c003b4023a7c6bf96a7310c7cd5f29d
Sandro Bormolini <sandro.bormolini@outlook.com>
2022-10-08 14:25:46 +0200

    Add unit test

---
+++ tests/api/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Ponos.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/web-blazor/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Ponos.Web.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
